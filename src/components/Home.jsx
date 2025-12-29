cfd3350a792c92298f3a53bb0890f80a355984f0
Pablo Cruz <151473902+pablo-cruzbr@users.noreply.github.com>
2025-12-29 11:48:02 -0300

    feat: upload img's section "home"

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,21 +1,25 @@
 import React from 'react'
-
+import BannerHomeBackground from "../assets/desing-fundo-home.png";
+import BannerHome from "../assets/desing-home.png"
 const Home = () => {
   return (
    <div className="home-container">
     <div className="home-banner-container">
         <div className="home-bannerImage-container">
-
+            <img src={BannerHomeBackground} alt="" />
         </div>
 
         <div className="home-text-section">
           <h1 className="primary-heading">
-           O sistema definitivo para empresas de informática dominarem a operação.
+           O sistema definitivo para empresas de informática dominarem a operação..
           </h1>
           <p className="primary-text">
             Do Helpdesk ao campo. Gerencie Ordens de Serviço, controle patrimônios e ofereça mobilidade total para seus técnicos com o Tech OS.
           </p>
-          
+        </div>
+
+        <div className="home-image-section">
+            <img src={BannerHome} alt="" />
         </div>
     </div>
    </div>
