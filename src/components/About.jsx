2f41c36b76ea93d38bd8a3f53af2837e52d25370
Pablo Cruz <151473902+pablo-cruzbr@users.noreply.github.com>
2025-12-29 11:11:22 -0300

    feat: create new section "Home, About"

---
+++ src/components/About.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className="about-section-container">
+        <div className="about-background-image-container">
+
+        </div>
+        <div className="about-section-text-container">
+            <p className="primary-subheading">About</p>
+            <h1 className="primary-heading">Food Is An Important Part Of A Balancce Diet</h1>
+            <p className="primary-text">
+          Lorem ipsum dolor sit amet consectetur. Non tincidunt magna non et
+          elit. Dolor turpis molestie dui magnis facilisis at fringilla quam.
+        </p>
+
+         <p className="primary-text">
+          Non tincidunt magna non et elit. Dolor turpis molestie dui magnis
+          facilisis at fringilla quam.
+        </p>
+        </div>
+    </div>
+  )
+}
+
+export default About
---
+++ src/components/Home.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+   <div className="home-container">
+    <div className="home-banner-container">
+        <div className="home-bannerImage-container">
+
+        </div>
+
+        <div className="home-text-section">
+          <h1 className="primary-heading">
+           O sistema definitivo para empresas de informática dominarem a operação.
+          </h1>
+          <p className="primary-text">
+            Do Helpdesk ao campo. Gerencie Ordens de Serviço, controle patrimônios e ofereça mobilidade total para seus técnicos com o Tech OS.
+          </p>
+          
+        </div>
+    </div>
+   </div>
+  )
+}
+
+export default Home
