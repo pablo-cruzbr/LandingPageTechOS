241c376029358adc2eb45542cbb31edad34a9164
Pablo Cruz <151473902+pablo-cruzbr@users.noreply.github.com>
2025-12-30 15:10:22 -0300

    feat: adjusting "margin-top" sections

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -4,7 +4,6 @@ const Testimonial = () => {
   return (
     <div className="work-section-wrapper">
       <div className="work-section-top">
-        <p className="primary-subheading">Construído com tecnologia de ponta para alta performance: </p>
         <h1 className="primary-heading">Construído com tecnologia de ponta para alta performance:</h1>
         <p className="primary-text">
          Utilizamos Next.js para uma interface ágil, Node.js para um backend robusto e PostgreSQL com Prisma para garantir a integridade e segurança dos seus dados.
@@ -15,9 +14,7 @@ const Testimonial = () => {
           <img src={ProfilePhoto} alt="" />
       <p>"Como técnico de helpdesk nível 2, senti na pele a dificuldade de gerenciar OS e ativos sem as ferramentas certas. Desenvolvi o Tech OS para ser o sistema que eu sempre quis usar: ágil, focado em mobilidade e com controle total sobre o que realmente importa na nossa rotina."</p>
 
-      <div className="textimonials-stars-container">
-        
-      </div>
+     
       <h2>Pablo Cruz</h2>
       </div>
     </div>
