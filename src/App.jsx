808e9334cd6a191b823c72f94a8cb3412ab8f44e
Pablo Cruz <151473902+pablo-cruzbr@users.noreply.github.com>
2026-01-29 11:50:07 -0300

    fix: component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import Home from './components/Home';
 import About from './components/About';
-import Work from './components/Work';
+import Work from './components/work';
 import Testimonial from './components/Testimonial';
 import AboutCalendar from './components/AboutCalendar';
 
